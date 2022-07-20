a99dee21ab7aa2e2edd4379c9608e670677bcd4f
Tydomx <michaelthai16@gmail.com>
2022-07-20 18:15:32 -0500

    Added query string parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,29 @@
 // reference to issue container
 var issueContainerEl = document.querySelector('#issues-container');
 var limitWarningEl = document.querySelector('#limit-warning');
+var repoNameEl = document.querySelector('#repo-name');
+
+var getRepoName = function () {
+	// grab repo name from url query string
+	var queryString = document.location.search;
+	var repoName = queryString.split('=')[1];
+
+	if (repoName) {
+		// display repo name on the page
+		repoNameEl.textContent = repoName;
+
+		getRepoIssues(repoName);
+	} else {
+		// if no repo given, redirect to homepage
+		document.location.replace('./index.html');
+	}
+};
 
 // does most of the work on single-repo.html page
 var getRepoIssues = function (repo) {
 	var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
 
+	// make get request to url
 	fetch(apiUrl).then(function (response) {
 		// request was successful
 		if (response.ok) {
@@ -19,7 +37,8 @@ var getRepoIssues = function (repo) {
 			});
 		}
 		else {
-			alert('There was a problem with your request!');
+			// if not successful redirect to homepage
+			document.location.replace('./index.html');
 		}
 	});
 };
@@ -78,4 +97,4 @@ var displayWarning = function (repo) {
 	limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues('facebook/react');
+getRepoName();
